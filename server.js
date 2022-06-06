bb24bfdb75abfdecc999eb4632ed11d59a91de8e
Ryan M. Waite <ryanwaite28@gmail.com>
2022-06-06 10:34:17 -0400

    added default build command

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ if (!isAppNameValid) {
 }
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + `/dist/${appName}`));
+app.use(express.static(__dirname + `./dist/${appName}`));
 
 app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, `./dist/${appName}/index.html`));
