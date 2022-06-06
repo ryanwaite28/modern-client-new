7c439a437fdeec4c5cef3520025bad273cc585cf
Ryan M. Waite <ryanwaite28@gmail.com>
2022-06-06 10:42:14 -0400

    server fixup

--- server.js
+++ server.js
@@ -19,11 +19,14 @@ if (!isAppNameValid) {
   process.exit(1);
 }
 
+const appDistRoot = __dirname + `/dist/${appName}`;
+console.log(`app dist root: ${appDistRoot}`);
+
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + `./dist/${appName}`));
+app.use(express.static(appDistRoot));
 
 app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, `./dist/${appName}/index.html`));
+  res.sendFile(`${appDistRoot}/index.html`);
 });
 
 // Start the app by listening on the default Heroku port
